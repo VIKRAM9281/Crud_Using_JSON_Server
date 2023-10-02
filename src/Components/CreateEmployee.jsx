59a1c96d49b9da7c2bc34f405bf817757e5019f8
VIKRAM9281 <vikrambhosale928@gmail.com>
2023-10-02 15:12:19 +0530

    gfh

--- src/Components/CreateEmployee.jsx
+++ src/Components/CreateEmployee.jsx
@@ -1,73 +1,93 @@
-import React, { useState } from 'react'
-import Navbar from './SubCompnents/Navbar'
-import {useNavigate} from "react-router-dom"
-import axios from 'axios'
-const CreateEmployee = () => {
-  const [name,setName]=useState("")
-  const [email,setEmail]=useState("")
-  const [Address,setAddress]=useState("")
-  const [Age,setAge]=useState("")
-  const [mobile,setMobile]=useState("")
-  const Navigate=useNavigate()
+import React, { useState } from 'react';
+import Navbar from './SubCompnents/Navbar';
+import { useNavigate } from 'react-router-dom';
+import axios from 'axios';
 
+const CreateEmployee = () => {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [Address, setAddress] = useState('');
+  const [Age, setAge] = useState('');
+  const [mobile, setMobile] = useState('');
+  const Navigate = useNavigate();
 
   const HandleSubmit = (e) => {
-  e.preventDefault();
-  const empobj = {
-    Name: name,
-    Email: email,
-    Address: Address,
-    Age: Age,
-    Mobile: mobile
+    e.preventDefault();
+    const empobj = {
+      Name: name,
+      Email: email,
+      Address: Address,
+      Age: parseInt(Age), // Convert Age to a number if it's supposed to be a number
+      Mobile: parseInt(mobile), // Convert Mobile to a number if it's supposed to be a number
+    };
+
+    axios
+      .post('http://localhost:5000/EmployeeData', empobj) // Send the object directly, no need to stringify it
+      .then((res) => {
+        console.log(res.data); // Assuming the response contains useful data
+        alert('Employee added successfully');
+        Navigate('/Employeelist');
+      })
+      .catch((error) => {
+        console.error(error);
+        alert('An error occurred while adding the employee.');
+      });
   };
-  console.log(empobj);
-
-  axios.post("http://localhost:5000/EmployeeData", JSON.stringify(empobj))
-    .then((res) => {
-      console.log(res.data); // Assuming the response contains useful data
-      alert("Employee added successfully");
-      // If you want to navigate after the request is complete, you should do it here.
-      // Assuming you have a function called Navigate.
-      Navigate("/Employeelist");
-    })
-    .catch((error) => {
-      console.error(error);
-      alert("An error occurred while adding the employee.");
-    });
-};
-
 
   return (
-   <>
-   <Navbar/>
-   <div className="contact-page">
-      <h2> * Create Employee *</h2>
-        <form action='' onSubmit={HandleSubmit}>
+    <>
+      <Navbar />
+      <div className="contact-page">
+        <h2>* Create Employee *</h2>
+        <form action="" onSubmit={HandleSubmit}>
           <label htmlFor="name">Name:</label>
-          <input type="text" id="name" name="name" autoComplete='off' value={name} onChange={(e) => setName(e.target.value)}/>
+          <input
+            type="text"
+            id="name"
+            name="name"
+            autoComplete="off"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
           <label htmlFor="email">Email:</label>
-          <input type="email" id="email" name="email"
-           autoComplete='off'
-           value={email} 
-           onChange={(e) => setEmail(e.target.value)} />
+          <input
+            type="email"
+            id="email"
+            name="email"
+            autoComplete="off"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
 
           <label htmlFor="address">Address:</label>
-          <input type="text" id="Address" name="Address"
-           autoComplete='off'
-           value={Address}
-           onChange={(e) => setAddress(e.target.value)} />
+          <input
+            type="text"
+            id="Address"
+            name="Address"
+            autoComplete="off"
+            value={Address}
+            onChange={(e) => setAddress(e.target.value)}
+          />
           <label htmlFor="Age">Age</label>
-          <input type="text" id="Age" name="Age"
-           autoComplete='off'
-           value={Age}
-           onChange={(e) => setAge(e.target.value)} />
+          <input
+            type="text"
+            id="Age"
+            name="Age"
+            autoComplete="off"
+            value={Age}
+            onChange={(e) => setAge(e.target.value)}
+          />
           <label htmlFor="message">Mobile</label>
-         <input type="text" value={mobile} onChange={(e) => setMobile(e.target.value)}/>
+          <input
+            type="text"
+            value={mobile}
+            onChange={(e) => setMobile(e.target.value)}
+          />
           <button type="submit">Submit</button>
         </form>
-    </div>
-   </>
-  )
-}
+      </div>
+    </>
+  );
+};
 
-export default CreateEmployee
+export default CreateEmployee;
